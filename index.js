b00ded7088b424338db87687231e7a994d5aa21e
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-05 16:16:33 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+var util = require('./util.js');
+var request = require('request').defaults({
+    baseUrl: 'https://api-ssl.bitly.com/'
+});
+
+var pickInputs = {
+        'shortUrl': 'shortUrl',
+        'hash': 'hash',
+        'expand_user': 'expand_user'
+    },
+    pickOutputs = {
+        'short_url': { keyName: 'data.info', fields: ['short_url'] },
+        'hash': { keyName: 'data.info', fields: ['hash'] },
+        'user_hash': { keyName: 'data.info', fields: ['user_hash'] },
+        'global_hash': { keyName: 'data.info', fields: ['global_hash'] },
+        'title': { keyName: 'data.info', fields: ['title'] },
+        'created_by': { keyName: 'data.info', fields: ['created_by'] },
+        'created_at': { keyName: 'data.info', fields: ['created_at'] }
+    };
+
 module.exports = {
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +27,25 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs),
+            token = dexter.environment('bitly_access_token'),
+            api = '/v3/info';
+
+        if (!token)
+            return this.fail('A [bitly_access_token] environment variable is required for this module');
+
+        if (validateErrors)
+            return this.fail(validateErrors);
+
+        inputs.access_token = token;
+        request.get({uri: api, qs: inputs, json: true}, function (error, response, body) {
+            if (error)
+                this.fail(error);
+            else if (body && body.status_code !== 200)
+                this.fail(body);
+            else
+                this.complete(util.pickOutputs(body, pickOutputs));
+        }.bind(this));
     }
 };
