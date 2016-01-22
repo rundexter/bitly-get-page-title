e0442077059798881cc02f2eee2ddc657bb217cd
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-22 17:40:58 +0200

    fix Oauth

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ var pickInputs = {
         'expand_user': 'expand_user'
     },
     pickOutputs = {
-        'short_url': { keyName: 'data.info', fields: ['short_url'] },
-        'hash': { keyName: 'data.info', fields: ['hash'] },
-        'user_hash': { keyName: 'data.info', fields: ['user_hash'] },
-        'global_hash': { keyName: 'data.info', fields: ['global_hash'] },
-        'title': { keyName: 'data.info', fields: ['title'] },
-        'created_by': { keyName: 'data.info', fields: ['created_by'] },
-        'created_at': { keyName: 'data.info', fields: ['created_at'] }
+        'short_url': { key: 'data.info', fields: ['short_url'] },
+        'hash': { key: 'data.info', fields: ['hash'] },
+        'user_hash': { key: 'data.info', fields: ['user_hash'] },
+        'global_hash': { key: 'data.info', fields: ['global_hash'] },
+        'title': { key: 'data.info', fields: ['title'] },
+        'created_by': { key: 'data.info', fields: ['created_by'] },
+        'created_at': { key: 'data.info', fields: ['created_at'] }
     };
 
 module.exports = {
@@ -29,21 +29,16 @@ module.exports = {
     run: function(step, dexter) {
         var inputs = util.pickInputs(step, pickInputs),
             validateErrors = util.checkValidateErrors(inputs, pickInputs),
-            token = dexter.environment('bitly_access_token'),
+            token = dexter.provider('bitly').credentials('access_token'),
             api = '/v3/info';
-
-        if (!token)
-            return this.fail('A [bitly_access_token] environment variable is required for this module');
 
         if (validateErrors)
             return this.fail(validateErrors);
 
         inputs.access_token = token;
         request.get({uri: api, qs: inputs, json: true}, function (error, response, body) {
-            if (error)
-                this.fail(error);
-            else if (body && body.status_code !== 200)
-                this.fail(body);
+            if (error || (body && body.status_code !== 200))
+                this.fail(error || body);
             else
                 this.complete(util.pickOutputs(body, pickOutputs));
         }.bind(this));
